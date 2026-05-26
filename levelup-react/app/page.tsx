8cd57e9ac04f0289c3323d1beedba929983668b1
minjae <minjagot@gmail.com>
2026-05-26 20:23:25 +0900

    작업 한 것 들 링크

--- levelup-react/app/page.tsx
+++ levelup-react/app/page.tsx
@@ -1,7 +1,14 @@
+import ButtonLink from "@/components/ButtonLink";
 import ItemList from "./item-list";
 export default function Home() {
   return (
     <main className="flex flex-col p-4">
+      <div className="flex gap-3">
+        <ButtonLink text="composition" url="/composition" />
+        <ButtonLink text="effect" url="/effect" />
+        <ButtonLink text="lifting-stateup" url="/lifting-stateup" />
+        <ButtonLink text="server-component" url="/server-component" />
+      </div>
       <ItemList />
     </main>
   );
---
+++ levelup-react/components/ButtonLink.tsx
@@ -0,0 +1,16 @@
+﻿import Link from "next/link";
+
+type ButtonLinkProps = {
+  text: string;
+  url: string;
+};
+
+const ButtonLink = ({ text, url }: ButtonLinkProps) => {
+  return (
+    <Link href={url}>
+      <button className="outline p-2 hover:bg-gray-300">{text}</button>
+    </Link>
+  );
+};
+
+export default ButtonLink;
